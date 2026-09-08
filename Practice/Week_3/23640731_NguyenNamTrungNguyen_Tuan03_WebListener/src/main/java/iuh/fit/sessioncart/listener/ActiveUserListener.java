12baabadd5c11bb068d4e5e9a84dade33c82d93f
TrungNguyen <23640731.nguyen@student.iuh.edu.vn>
2026-09-08 17:28:07 +0700

    update code lab 2 week 3

--- Practice/Week_3/23640731_NguyenNamTrungNguyen_Tuan03_WebListener/src/main/java/iuh/fit/sessioncart/listener/ActiveUserListener.java
+++ Practice/Week_3/23640731_NguyenNamTrungNguyen_Tuan03_WebListener/src/main/java/iuh/fit/sessioncart/listener/ActiveUserListener.java
@@ -14,28 +14,32 @@ public class ActiveUserListener implements ServletContextListener, HttpSessionLi
     private static final String ACTIVE_USERS_ATTRIBUTE = "activeUsersCount";
     // Khởi tạo biến đếm = 0 khi server (application) bắt đầu chạy
     private static final AtomicInteger activeSessions = new AtomicInteger(0);
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         sce.getServletContext().setAttribute(ACTIVE_USERS_ATTRIBUTE,
                 activeSessions);
     }
+    
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-// Dọn dẹp nếu cần khi server tắt
+        // Dọn dẹp nếu cần khi server tắt
         sce.getServletContext().removeAttribute(ACTIVE_USERS_ATTRIBUTE);
     }
+
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-// Mỗi khi có session mới (1 người dùng mới truy cập), tăng biến đếm lên 1
+        // Mỗi khi có session mới (1 người dùng mới truy cập), tăng biến đếm lên 1
         AtomicInteger activeUsers = (AtomicInteger)
                 se.getSession().getServletContext().getAttribute(ACTIVE_USERS_ATTRIBUTE);
         if (activeUsers != null) {
             activeUsers.incrementAndGet();
         }
     }
+
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-// Khi session timeout hoặc bị hủy (người dùng đăng xuất/thoát), giảm biến đếm đi 1
+        // Khi session timeout hoặc bị hủy (người dùng đăng xuất/thoát), giảm biến đếm đi 1
         AtomicInteger activeUsers = (AtomicInteger)
                 se.getSession().getServletContext().getAttribute(ACTIVE_USERS_ATTRIBUTE);
         if (activeUsers != null && activeUsers.get() > 0) {
